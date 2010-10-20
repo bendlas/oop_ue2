70b8a9cf2eebb958891bb2d92c00ff07d577ef92
noce <e0827181@student.tuwien.ac.at>
2010-10-20 23:13:23 +0200

    company + orders

--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,31 @@
+import java.util.Date;
+import java.util.HashMap;
+
 
 public class Order {
+	protected Date inTime;
+	protected Date outTime;
+	protected HashMap<TradeItem, Integer> order; 
+	protected Store source;
+	protected Store destination;
+	
+	public Order(Date in, Date out, HashMap<TradeItem, Integer> order, Store source, Store destination){
+		this.inTime = in;
+		this.outTime = out;
+		this.order = order;
+		this.source = source;
+		this.destination = destination;
+	}
+	
+	public void executeOrder(){
+		
+	}
+	
+	public void deleteOrder(){
+		
+	}
+	
 
 }
+	
+
