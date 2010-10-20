905f61d3c04105a56e5e5ea0297443dfa07b32e6
babz <babz@babz-laptop.(none)>
2010-10-20 21:41:50 +0200

    class tradeitem

--- src/TradeItem.java
+++ src/TradeItem.java
@@ -1,4 +1,33 @@
+import java.math.BigDecimal;
 
 public abstract class TradeItem {
-
+	
+	protected BigDecimal buyPrice, sellPrice, storeCosts;
+	
+	public TradeItem(BigDecimal buyPrice, BigDecimal sellPrice, BigDecimal storeCosts){
+		this.buyPrice = buyPrice;
+		this.sellPrice = sellPrice;
+		this.storeCosts = storeCosts;
+	}
+	
+	public BigDecimal getBuyPrice(){
+		return buyPrice;
+	}
+	public void setBuyPrice(BigDecimal buyP){
+		buyPrice = buyP;
+	}
+	
+	public BigDecimal getSellPrice(){
+		return sellPrice;
+	}
+	public void setSellPrice(BigDecimal sellP){
+		sellPrice = sellP;
+	}
+	
+	public BigDecimal getStoreCosts(){
+		return storeCosts;
+	}
+	public void setStoreCosts(BigDecimal storeC){
+		storeCosts = storeC;
+	}
 }
